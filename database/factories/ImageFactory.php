a1259d9d0dabd7d6c080393b523c2e963dcefd26
haraltfunk <haraltfunk@gmail.com>
2021-05-03 11:41:40 -0400

    sin PermissinSeeder

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -22,7 +22,7 @@ class ImageFactory extends Factory
     public function definition()
     {
         return [
-            'url' => 'courses/' . $this->faker->image('public/storage/courses', 640, 480, null, false), // courses/imagen.jpg
+            'url' => 'courses/' . $this->faker->image('public/storage/courses', 640, 480, null, false),
             'imageable_id' => null,
             'imageable_type' => null
         ];
